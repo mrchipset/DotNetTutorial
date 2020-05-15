7602122d78a0f9449db97cf7502b1812d7b392da
X.F.Zhou <zhouxinfeng@srlab.com.cn>
2020-05-15 14:52:21 +0800

    FEAT: add a MongoDB getting start and list all dbs

---
+++ MongoDBDemo/MongoDBHelper.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBDemo
+{
+    class MongoDBHelper
+    {
+        private MongoClient client;
+        public MongoDBHelper(string serverAddr, string portNum)
+        {
+            string connectString = "mongodb://" + serverAddr + ":" + portNum;
+            client = new MongoClient(connectString);
+        }
+
+        public void ListDataBases()
+        {
+            var dbList = client.ListDatabases().ToList();
+            Console.WriteLine("Following shows the dbs in List:");
+            foreach(var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+
+        public IMongoDatabase GetDatabase(string dbName)
+        {
+            return client.GetDatabase(dbName);
+        }
+
+
+    }
+}
---
+++ MongoDBDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB;
+using MongoDB.Driver;
+namespace MongoDBDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoDBHelper client = new MongoDBHelper("localhost", "27017");
+            client.ListDataBases();
+            Console.ReadKey();
+        }
+    }
+}
